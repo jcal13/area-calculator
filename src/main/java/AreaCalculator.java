803fc11064e2447ac970c60e31a4cd0d1e7bc5b2
Jonathan Calver <calver@cs.toronto.edu>
2025-10-02 10:02:41 -0400

    added Circle and generalized calculateArea to handle circles and rectangles

--- src/main/java/AreaCalculator.java
+++ src/main/java/AreaCalculator.java
@@ -1,9 +1,17 @@
 public class AreaCalculator {
 
-    public static double calculateArea(Rectangle[] rectangles) {
+    public static double calculateArea(Object[] objects) {
         double totalArea = 0;
-        for (Rectangle rectangle : rectangles) {
-            totalArea += rectangle.getWidth() * rectangle.getHeight();
+        for (Object obj : objects) {
+            if (obj instanceof Circle) {
+                Circle circle = (Circle) obj;
+                totalArea = totalArea + Math.pow(circle.getRadius(), 2) * Math.PI;
+            } else if (obj instanceof Rectangle) {
+                Rectangle rectangle = (Rectangle) obj;
+                totalArea += rectangle.getWidth() * rectangle.getHeight();
+            }
+            //ignore if not one of these shapes
+
         }
         return totalArea;
     }
--- src/test/java/AreaCalculatorTest.java
+++ src/test/java/AreaCalculatorTest.java
@@ -11,4 +11,12 @@ class AreaCalculatorTest {
         rectangles[1] = new Rectangle(5, 5);
         assertEquals(50, AreaCalculator.calculateArea(rectangles), 0);
     }
+
+    @Test
+    void calculateAreaMixed() {
+        Object[] objects = new Object[2];
+        objects[0] = new Rectangle(5, 5);
+        objects[1] = new Circle(5);
+        assertEquals( 25 + 25 * Math.PI, AreaCalculator.calculateArea(objects), 0);
+    }
 }
